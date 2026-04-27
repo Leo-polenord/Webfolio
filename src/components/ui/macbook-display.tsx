ce54abda5e9f9cfe01c913a628ffde368bbdbdfd
Leo-polenord <lapoule.jay@gmail.com>
2026-04-27 23:25:54 +0200

    🚧 WIP: : adjust layout in MacBookDisplay for improved structure and readability

--- src/components/ui/macbook-display.tsx
+++ src/components/ui/macbook-display.tsx
@@ -17,17 +17,21 @@ export default function MacBookDisplay() {
             className="w-full h-auto"
           />
 
-          <div className="absolute top-[10%] left-[10%] w-[80%] h-[80%] overflow-y-auto overflow-x-hidden rounded-t-[20px] border-[10px] border-black">
+          <div className="absolute top-[10%] left-[10%] w-[80%] h-[80%] overflow-hidden rounded-t-[20px] border-[10px] border-black">
             <div className="absolute top-0 left-1/2 -translate-x-1/2 w-28 h-5 bg-black rounded-b-lg z-[60]" />
             <div className="h-full overflow-y-auto overflow-x-hidden">
-            <div className="pt-12">
-              <FloatingNavbar />
-            </div>
-            <HeroSection />
-            <SkillsSection />
-            <ProjectsSection />
-            <AboutSection />
-            <ContactSection />
+              <div className="pt-8">
+                <FloatingNavbar />
+              </div>
+              <div className="h-[calc(100%-60px)]">
+                <HeroSection />
+              </div>
+              <div className="scale-75 origin-top pb-4">
+                <SkillsSection />
+                <ProjectsSection />
+                <AboutSection />
+                <ContactSection />
+              </div>
             </div>
           </div>
         </div>
